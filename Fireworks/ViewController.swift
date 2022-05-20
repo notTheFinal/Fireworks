2320d71fdfbdc7953c438af13ab7c6abb46c8869
JlblCblU4EPT <sashabarsasha228@yandex.ru>
2022-05-20 15:22:57 +0800

    Initial Commit

---
+++ Fireworks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fireworks
+//
+//  Created by Александр on 20.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
